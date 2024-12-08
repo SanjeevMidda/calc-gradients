4403396abc2383fc1e9b2eacd174a29ab9f64b69
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-12-08 22:24:50 +0000

    Add files via upload
    Uploading source files.

---
+++ calc-gradients/src/App.jsx
@@ -0,0 +1,27 @@
+import Block from "./components/Block";
+
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+
+      <div className="blockContainer">
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+        <div className="block"></div>
+      </div>
+
+      {/* <Block />
+      <Block />
+      <Block />
+      <Block />
+      <Block /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ calc-gradients/src/components/Block.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Block = () => {
+  return <div className="block">Block</div>;
+};
+
+export default Block;
